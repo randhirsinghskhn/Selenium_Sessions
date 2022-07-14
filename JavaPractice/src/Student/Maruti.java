31bb60384f7666dd9c0062ef0cfbf7b6aa79106c
randh <randh@TABLET-336T4SBR>
2022-07-14 11:35:58 +0530

    committing inheritance programs with this and super keyword.

---
+++ JavaPractice/src/Student/Maruti.java
@@ -0,0 +1,29 @@
+package Student;
+
+public class Maruti {
+
+	public Maruti()
+	{
+		this(8,6,5);
+		System.out.println("Parent Default constructor.");
+	}
+	public Maruti(int a)
+	{
+		this(6,7);
+		System.out.println("Parent one parameterized constructor.");
+	}
+	public Maruti(int a,int b)
+	{
+		this(2,5,7,9);
+		System.out.println("Parent two parameterized constructor.");
+	}
+	public Maruti(int a,int b,int c)
+	{
+		System.out.println("Parent three parameterized constructor.");
+	}
+	public Maruti(int a,int b,int c,int d)
+	{
+		this();
+		System.out.println("Parent four parameterized constructor.");
+	}
+}
---
+++ JavaPractice/src/Student/Swift.java
@@ -0,0 +1,33 @@
+package Student;
+
+public class Swift extends Maruti {
+
+	public Swift()
+	{
+		this(5,7);
+		System.out.println("Child default constructor");
+	}
+	public Swift(int a)
+	{
+		this(3,4,5,6);
+		System.out.println("Child one parametrized constructor");
+	}
+	public Swift(int a, int b)
+	{
+		super(5);
+		System.out.println("Child two parametrized constructor");
+	}
+	public Swift(int a, int b, int c)
+	{
+		this();
+		System.out.println("Child three parametrized constructor");
+	}
+	public Swift(int a, int b, int c, int d)
+	{
+		this(5,6,7);
+		System.out.println("Child four parametrized constructor");
+	}
+	public static void main(String[] args) {
+		Swift ob=new Swift(3);
+	}
+}
